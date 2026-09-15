0201238eea286207f10d182343fa26fdc04bab60
Niccolo Terreri <niccolo.terreri@mindlinksoft.com>
2026-09-15 15:46:23 +0100

    add command support

---
+++ Rover/Command.cs
@@ -0,0 +1,7 @@
+﻿namespace Rover;
+
+public enum Command
+{
+    F,
+    B
+}
--- WakingSkeleton.Test/RoverTest.cs
+++ WakingSkeleton.Test/RoverTest.cs
@@ -22,11 +22,11 @@ public class Tests
     }
 
     [Test]
-    [TestCase(Direction.North, 1, 2)]
-    [TestCase(Direction.West, 0, 1)]
-    [TestCase(Direction.East, 2, 1)]
-    [TestCase(Direction.South, 1, 0)]
-    public void MovingRoverForwardMovesForwardByOne(Direction startingDirection, int expectedX, int expectedY)
+    [TestCase(Direction.North, 1, 2, Command.F)]
+    [TestCase(Direction.West, 0, 1, Command.F)]
+    [TestCase(Direction.East, 2, 1, Command.F)]
+    [TestCase(Direction.South, 1, 0, Command.F)]
+    public void MovingRoverForwardMovesByOne(Direction startingDirection, int expectedX, int expectedY, Command command)
     {
         var rover = new Rover(1, 1, startingDirection);
 
