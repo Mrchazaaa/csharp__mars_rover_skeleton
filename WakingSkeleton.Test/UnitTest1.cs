5549660ef45182a9a9a05120f86304d63fecdc46
Charlie Howlett <charlie.howlett@mindlinksoft.com>
2026-09-15 15:02:03 +0100

    rover initialization

--- WakingSkeleton.Test/UnitTest1.cs
+++ WakingSkeleton.Test/UnitTest1.cs
@@ -2,10 +2,33 @@ namespace WakingSkeleton.Test;
 
 public class Tests
 {
+    public class Rover {
+        public int x;
+        public int y;
+        public string direction;
+
+        public Rover(int x, int y, string direction) {
+          this.x = x;
+          this.y = y;
+          this.direction = direction;
+        }
+    }
+
 
     [Test]
-    public void Test1()
+    public void InitializingRoverSetsPosition()
     {
-        Assert.Pass();
+        var x = 2;
+        var y = 3;
+        var direction = "North";
+
+        var rover = new Rover(
+            x,
+            y,
+            direction);
+
+        Assert.That(rover.x, Is.EqualTo(x));
+        Assert.That(rover.y, Is.EqualTo(y));
+        Assert.That(rover.direction, Is.EqualTo(direction));
     }
 }
