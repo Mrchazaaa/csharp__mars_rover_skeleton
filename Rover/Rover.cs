af6786aab1cb13e7c441ec819f34e5b491b1fc2d
Niccolo Terreri <niccolo.terreri@mindlinksoft.com>
2026-09-15 16:12:21 +0100

    add backwards test

--- Rover/Rover.cs
+++ Rover/Rover.cs
@@ -13,26 +13,53 @@ public sealed class Rover
         this.Direction = direction;
     }
 
-    public void ReceiveCommand()
+    public void ReceiveCommand(Command command)
     {
-        switch(this.Direction)
+        switch (command)
         {
-          case Direction.North:
-            this.x=1;
-            this.y=2;
-            break;
-          case Direction.East:
-            this.x=2;
-            this.y=1;
-            break;
-          case Direction.South:
-            this.x=1;
-            this.y=0;
-            break;
-          case Direction.West:
-            this.x=0;
-            this.y=1;
-            break;
+            case Command.F:
+                switch (this.Direction)
+                {
+                    case Direction.North:
+                        this.x = 1;
+                        this.y = 2;
+                        break;
+                    case Direction.East:
+                        this.x = 2;
+                        this.y = 1;
+                        break;
+                    case Direction.South:
+                        this.x = 1;
+                        this.y = 0;
+                        break;
+                    case Direction.West:
+                        this.x = 0;
+                        this.y = 1;
+                        break;
+                }
+                break;
+            case Command.B:
+                switch (this.Direction)
+                {
+                    case Direction.North:
+                        this.x = 1;
+                        this.y = 0;
+                        break;
+                    case Direction.East:
+                        this.x = 0;
+                        this.y = 1;
+                        break;
+                    case Direction.South:
+                        this.x = 1;
+                        this.y = 2;
+                        break;
+                    case Direction.West:
+                        this.x = 2;
+                        this.y = 1;
+                        break;
+                }
+                break;
         }
+
     }
 }
--- WakingSkeleton.Test/RoverTest.cs
+++ WakingSkeleton.Test/RoverTest.cs
@@ -23,14 +23,18 @@ public class Tests
 
     [Test]
     [TestCase(Direction.North, 1, 2, Command.F)]
-    [TestCase(Direction.West, 0, 1, Command.F)]
     [TestCase(Direction.East, 2, 1, Command.F)]
     [TestCase(Direction.South, 1, 0, Command.F)]
+    [TestCase(Direction.West, 0, 1, Command.F)]
+    [TestCase(Direction.North, 1, 0, Command.B)]
+    [TestCase(Direction.East, 0, 1, Command.B)]
+    [TestCase(Direction.South, 1, 2, Command.B)]
+    [TestCase(Direction.West, 2, 1, Command.B)]
     public void MovingRoverForwardMovesByOne(Direction startingDirection, int expectedX, int expectedY, Command command)
     {
         var rover = new Rover(1, 1, startingDirection);
 
-        rover.ReceiveCommand();
+        rover.ReceiveCommand(command);
 
         Assert.That(rover.x, Is.EqualTo(expectedX));
         Assert.That(rover.y, Is.EqualTo(expectedY));
