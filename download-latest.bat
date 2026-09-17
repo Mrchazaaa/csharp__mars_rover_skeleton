03d3078f1cfa2caf6d084b427b1e650bb422443e
Charlie Howlett <charlie.howlett@mindlinksoft.com>
2026-09-17 12:00:18 +0100

    pushing release pipeline

---
+++ download-latest.bat
@@ -0,0 +1,4 @@
+@echo off
+curl -fL -o MarsRover_TeamA.exe "https://github.com/Mrchazaaa/csharp__mars_rover_skeleton/releases/latest/download/MarsRover_TeamA.exe" || goto :eof
+MarsRover_TeamA.exe
+pause
--- download-latest.ps1
+++
@@ -1,1 +0,0 @@
-Invoke-WebRequest "https://github.com/Mrchazaaa/csharp__mars_rover_skeleton/releases/latest/download/MarsRover_TeamA.exe" -OutFile "MarsRover_TeamA.exe"
