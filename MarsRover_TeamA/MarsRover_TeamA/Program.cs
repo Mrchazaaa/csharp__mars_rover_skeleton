09fe0b1c9acd05e2abf02db39d114e83b06b6446
Reid Henwick <reid.henwick@mindlinksoft.com>
2026-09-17 11:45:50 +0100

    initial entry point

--- MarsRover_TeamA/MarsRover_TeamA/Program.cs
+++ MarsRover_TeamA/MarsRover_TeamA/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace MarsRover_TeamA;
+﻿using System.Transactions;
+
+namespace MarsRover_TeamA;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the Mars Rover Program!");
+        Console.WriteLine("Please enter your initial position in formate X Y D (e.g., 1 2 N)");
+        var initalInput = Console.ReadLine();
     }
 }
